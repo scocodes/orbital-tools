d583046ea71dc41aae9ddb9fe02ec8a64b318065
Scott Collins <scottjcollins19@gmail.com>
2026-05-16 21:34:04 +0100

    Added RK4 integrator

---
+++ src/integrators.py
@@ -0,0 +1,11 @@
+
+def rk4_step(fun, t, y, dt, **args):
+
+    k1 = fun(t, y, args)
+    k2 = fun(t + dt/2, y + dt/2 * k1, args)
+    k3 = fun(t + dt/2, y + dt/2 * k2, args)
+    k4 = fun(t + dt, y + dt * k3, args)
+
+    kavg = (1/6) * (k1 + 2*k2 + 2*k3 + k4)
+
+    return y + dt * kavg
