4b24d1dab7cbd19930ebcdfdd549985155ce516b
Scott Collins <scottjcollins19@gmail.com>
2026-05-28 16:32:16 +0100

    Updated RK4 arguments

--- src/orbital_tools/integrators.py
+++ src/orbital_tools/integrators.py
@@ -1,9 +1,9 @@
-def rk4_step(fun, t, y, dt, **args):
+def rk4_step(fun, t, y, dt, **kwargs):
 
-    k1 = fun(t, y, args)
-    k2 = fun(t + dt/2, y + dt/2 * k1, args)
-    k3 = fun(t + dt/2, y + dt/2 * k2, args)
-    k4 = fun(t + dt, y + dt * k3, args)
+    k1 = fun(t, y, **kwargs)
+    k2 = fun(t + dt/2, y + dt/2 * k1,  **kwargs)
+    k3 = fun(t + dt/2, y + dt/2 * k2,  **kwargs)
+    k4 = fun(t + dt, y + dt * k3,  **kwargs)
 
     kavg = (1/6) * (k1 + 2*k2 + 2*k3 + k4)
 
